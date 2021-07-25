d3aba03555497e638c7d6148de58f6f059065086
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-25 22:23:35 +0900

    【javascript】画像を表示するためのHTML要素を生成

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,6 +2,14 @@ window.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
   document.getElementById('message_image').addEventListener('change', function(e){
     const file = e.target.files[0];
+
+    // URLオブジェクト内の画像から画像URLを生成する
     const blob = window.URL.createObjectURL(file);
+
+    //画像を表示するための div要素を生成
+    const imageElement = document.createElement('div');
+
+    //表示する画像imgを生成
+    const blobImage = document.createElement('img');
   });
 });
