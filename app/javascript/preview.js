5427caf6d75e299d6f4cf1a100a2c6dd3fbf58bd
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-26 18:51:49 +0900

    【javascript】複数画像選択のためのinputHTMLとimageElementNum

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -6,10 +6,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const createImageHTML = (blob) => {
        // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div')
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
 
       // 表示する画像を生成
       const blobImage = document.createElement('img')
       blobImage.setAttribute('src', blob)
+
+      //ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input')
+
+      //作った input要素に属性をつける
+      inputHTML.setAttribute('id', `message_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'message[images][]')
+      inputHTML.setAttribute('type', 'file')
+      // ↑ で完成するHTML要素はこちら <input id="message_image_[番号]" name="message[images][]" type="file">
+
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage)
       ImageList.appendChild(imageElement)
